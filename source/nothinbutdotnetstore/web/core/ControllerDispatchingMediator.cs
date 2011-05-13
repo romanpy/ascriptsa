8657e356849e9b983b071aa9ead476fde499fb99
romanpy <rpysmen@hotmail.com>
2011-05-13 11:15:30 -0400

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/ControllerDispatchingMediator.cs
+++ source/nothinbutdotnetstore/web/core/ControllerDispatchingMediator.cs
@@ -4,9 +4,16 @@ namespace nothinbutdotnetstore.web.core
 {
   public class ControllerDispatchingMediator : IProcessApplicationSpecificBehaviour
   {
+    public ControllerDispatchingMediator(ICanFindAControllerToProcessARequest registry)
+    {
+        this.registry = registry;
+    }
+
+    private ICanFindAControllerToProcessARequest registry;
+
     public void run(IContainRequestInformation request)
     {
-      throw new NotImplementedException();
+        registry.get_the_controller_that_can_handle(request);
     }
   }
 }
